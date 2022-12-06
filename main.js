ce6e8a2bdbaa3edc96223ee545b5a1fe7eff3706
Youmeng <youmenghin15@gmail.com>
2022-12-06 18:42:26 -0500

    reworked base code

--- main.js
+++ main.js
@@ -1,88 +1,137 @@
-var camera, scene, renderer, material;
-var meshes = [];
-const geometry0 = new THREE.PlaneGeometry(1000,1000);
-const base = new THREE.MeshBasicMaterial({color: 0x454B1B, side: THREE.DoubleSide});
+var camera, scene, renderer, meshes = [], materials = [], geometries = [];
+const mouse = new THREE.Vector2();
+const target = new THREE.Vector2();
+const windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+
 init();
 
 function init() {
     // Renderer.
     renderer = new THREE.WebGLRenderer();
-    //renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    // renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
     // Add renderer to page
     document.body.appendChild(renderer.domElement);
-
-    // Create camera.
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(400,0,600);
 
     // Create scene.
     scene = new THREE.Scene();
 
-    // Create material
-    material = new THREE.MeshPhongMaterial();
+    // Create camera.
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(0, 0, 600);
+    scene.add(camera);
 
-    // Create cube and add to scene.
-    var geometry = new THREE.BoxGeometry(200, 200, 200);
+    // Create materials
+    var mat = new THREE.MeshPhongMaterial({ color: 0xff6208 });   // material 1
+    materials.push(mat);
+    mat = new THREE.MeshPhongMaterial({ color: 0xc9c42e });       // material 2
+    materials.push(mat);
+    mat = new THREE.MeshPhongMaterial({ color: 0x3424bf });       // material 3
+    materials.push(mat);
+    mat = new THREE.MeshPhongMaterial({ color: 0x700099 });       // material 4
+    materials.push(mat)
 
-    for(var i = 0; i<5; i++) {
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(250*i,0,0);
+    // Create geometry obejcts
+    var geometry = new THREE.BoxGeometry(100, 100, 100);    // geometry object 1
+    geometries.push(geometry);
+    geometry = new THREE.BoxGeometry(100, 100, 100);        // geometry object 2
+    geometries.push(geometry);
+    geometry = new THREE.BoxGeometry(100, 100, 100);        // geometry object 3
+    geometries.push(geometry);
+    geometry = new THREE.BoxGeometry(100, 100, 100);        // geometry object 4
+    geometries.push(geometry)
+
+    // Add objects to scene
+    for (var i = 0; i < 4; i++) {
+        mesh = new THREE.Mesh(geometries[i], materials[i]);
+        mesh.position.set((200 * i) - 300, 0, 0);
         scene.add(mesh);
         meshes.push(mesh);
     }
 
-    //Becker
-    const plane = new THREE.Mesh(geometry0, base);
-    plane.position.set(0,-200,0);
-    plane.rotateX(1.5708);
-    scene.add(plane);
-
-    // Create ambient light and add to scene.
-    var light = new THREE.AmbientLight(0x404040); // soft white light
-    scene.add(light);
-
-    // Create directional light and add to scene.
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(2, 1, 1).normalize();
+    // Create lights and add to scene
+    var ambientLight = new THREE.AmbientLight(0x404040);                        // ambient light
+    scene.add(ambientLight);
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 1.25);          // directional light 1
+    directionalLight.position.set(1, 1, 1).normalize();
     scene.add(directionalLight);
+    directionalLight = new THREE.DirectionalLight(0xf7bd52, 0.75);              // directional light 2
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
+    var hemisphereLight = new THREE.HemisphereLight(0x95ebf0, 0xed9118, 0.5);   // hemisphere light
+    scene.add(hemisphereLight);
+
+    // Add ground (plane geometry object)
+    var geometry = new THREE.PlaneGeometry(2000, 2000, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x089c1e });
+    var ground = new THREE.Mesh(geometry, material);
+    ground.material.side = THREE.DoubleSide;
+    ground.rotation.x = 90;
+    ground.position.set(0, -500, 0)
+    scene.add(ground);
+
+    // Add background (plane geometry object)
+    var geometry = new THREE.PlaneGeometry(2000, 2000, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x086ec2 });
+    var ground = new THREE.Mesh(geometry, material);
+    ground.material.side = THREE.DoubleSide;
+    ground.position.set(0, 0, -500)
+    scene.add(ground);
 
     // Add listener for window resize.
     window.addEventListener('resize', onWindowResize, false);
 
     // Add listener for keyboard
-    document.body.addEventListener('keydown', keyPressed, false);
+    document.body.addEventListener('keydown', onkeyPressed, false);
+
+    //document.addEventListener for mouse zoom;
+    document.addEventListener('wheel', onMouseWheel, false);
 
     render();
-
 }
 
-function keyPressed(e){
-    switch(e.key) {
+function onMouseWheel(e) {
+    camera.position.z += e.deltaY * 0.2;
+}
+
+function onkeyPressed(e) {
+    switch (e.key) {
         case 'ArrowUp':
-            meshes[0].position.y += 10;
+            camera.rotateX(0.1);
             break;
         case 'ArrowDown':
-            meshes[0].position.y -= 10;
+            camera.rotateX(-0.1);
             break;
         case 'ArrowLeft':
-            meshes[0].position.x -= 10;
+            camera.rotateY(0.1);
             break;
         case 'ArrowRight':
-            meshes[0].position.x += 10;
+            camera.rotateY(-0.1);
+            break;
+        case 'w':
+            camera.position.z -= 10;
+            break;
+        case 's':
+            camera.position.z += 10;
+            break;
+        case 'a':
+            camera.position.x -= 10;
+            break;
+        case 'd':
+            camera.position.x += 10;
             break;
     }
     e.preventDefault();
     render();
 }
 
-function render(){
-    renderer.render(scene, camera);
-}
-
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+}
+
+function render() {
     renderer.render(scene, camera);
 }
